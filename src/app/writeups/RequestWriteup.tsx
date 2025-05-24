d9c52f6c3950e55f572085414ce92c11c6d22b5c
Ang Leng Khai <lengkhai@gmail.com>
2025-05-24 13:40:37 +0800

    fix: char

--- src/app/writeups/RequestWriteup.tsx
+++ src/app/writeups/RequestWriteup.tsx
@@ -1,8 +1,8 @@
 export default function RequestWriteup() {
     return <div>
         <span>
-            Thank you so much for engaging with me! I really appreciate your song requests and will try my best to fulfil every single request.
-            If your song has not been called for a while, don't hesitate to request another song! Thank you so much for your understanding :)
+            {`Thank you so much for engaging with me! I really appreciate your song requests and will try my best to fulfil every single request.
+            If your song has not been called for a while, don't hesitate to request another song! Thank you so much for your understanding :)`}
         </span>
     </div>
 
